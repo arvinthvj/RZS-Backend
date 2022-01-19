fd6acecb31c66b73ebff73630d0037aa111f35d6
arvinthinnive <amanivannaboopathy@inniveinc.com>
2022-01-20 01:01:53 +0530

    added delete route and bug fixes

--- controllers/twitterController.js
+++ controllers/twitterController.js
@@ -1,24 +1,40 @@
 const { default: axios } = require("axios");
 const Oauth1Helper = require("./OAuthSupporter/OauthMaker");
+//OAuth Maker:
 
-
-
-const sendATweet =async(req, res, next)=>{
-	const request = {
-		url: 'https://api.twitter.com/2/tweets',
-		method: 'POST',
-		body: {
-			"text": req.body.text
-		}
-	};
-	const authHeader = Oauth1Helper.getAuthHeaderForRequest(request);
-	 await axios.post(
-		request.url,
-		request.body,
-		{ headers: authHeader });
-		res.send("Tweet sent successfully !!")
+const makeAuth =(request)=>{
+	return Oauth1Helper.getAuthHeaderForRequest(request);
 }
 
+const sendATweet = async (req, res, next) => {
+  const request = {
+    url: "https://api.twitter.com/2/tweets",
+    method: "POST",
+    body: {
+      text: req.body.text,
+    },
+  };
+  
+  try {
+    await axios.post(request.url, request.body, { headers: authHeader });
+    res.send("Tweet sent successfully !!");
+  } catch (error) {
+    res.send(error);
+  }
+};
 
+const deleteATweetById = async (req, res, next) => {
+  try {
+	  debugger
+	const request = {
+		url: `https://api.twitter.com/2/tweets/${req.params.id}`,
+		method: "DELETE"
+	  };
+    await axios.delete(request.url,{ headers: makeAuth(request) });
+    res.send(`Tweet with the id : ${req.params.id} deleted successfully !!`);
+  } catch (error) {
+    res.send(error);
+  }
+};
 
-module.exports ={sendATweet} 
+module.exports = { sendATweet ,deleteATweetById};
--- routes/twitterRoutes.js
+++ routes/twitterRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const {sendATweet } = require("../controllers/twitterController");
+const {sendATweet, deleteATweetById } = require("../controllers/twitterController");
 
 const twitterRouter = express.Router();
 
 twitterRouter.post('/tweetIt', sendATweet);
+twitterRouter.delete('/deleteById/:id', deleteATweetById);
 
 module.exports = {
   twitterRoutes: twitterRouter,
